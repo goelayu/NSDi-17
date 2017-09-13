8dc29b393f3bf8fa63431d9f5d19502840034f98
Muhammed Uluyol <uluyol0@gmail.com>
2017-09-13 09:47:40 -0400

    make pylint happy, clean up, bugfixes
    Map was formatted with jq. Missing import. Consistently use tabs.
    Convert to float on load not lazily and ignore non-floats (logs
    have some jumbled output due to races).

--- comparing_latencies.py
+++ comparing_latencies.py
@@ -1,17 +1,66 @@
 #!/usr/bin/env python
 
+import errno
 import sys
 from os import listdir
 import xml.etree.ElementTree as et
 import random
 import os
 
-networkLatencies='/vault-home/goelayu/NSDI17/goelayu/with-missing-gc-reg/located-ping-times.txt'
-storageLatencies='/vault-home/uluyol/paxosstore-results/sm-100K/'
-storageLatencyReadFile = 'store-reads.logs'
-storageLatencyWriteFile = 'store-writes.logs'
+NET_LATENCIES_LOG = '/vault-home/goelayu/NSDI17/goelayu/with-missing-gc-reg/located-ping-times.txt'
+STORE_LATENCIES_TOP = '/vault-home/uluyol/paxosstore-results/sm-100K/'
+STORE_READ_FILE = '/store-reads.log'
+STORE_WRITE_FILE = '/store-writes.log'
 
-dcIndexMap = { '0' : 'aws/ap-northeast-1','1' : 'aws/ap-northeast-2','2' : 'aws/ap-south-1','3' : 'aws/ap-southeast-1','4' : 'aws/ap-southeast-2','5' : 'aws/ca-central-1','6' : 'aws/eu-central-1','7' : 'aws/eu-west-1','8' : 'aws/eu-west-2','9' : 'aws/sa-east-1','10' : 'aws/us-east-1','11' : 'aws/us-east-2','12' : 'aws/us-west-1','13' : 'aws/us-west-2','14' : 'az/australiaeast','15' : 'az/australiasoutheast','16' : 'az/brazilsouth','17' : 'az/canadacentral','18' : 'az/canadaeast','19' : 'az/centralindia','20' : 'az/centralus','21' : 'az/eastasia','22' : 'az/eastus','23' : 'az/eastus2','24' : 'az/japaneast','25' : 'az/japanwest','26' : 'az/koreacentral','27' : 'az/koreasouth','28' : 'az/northcentralus','29' : 'az/northeurope','30' : 'az/southcentralus','31' : 'az/southeastasia','32' : 'az/southindia','33' : 'az/uksouth','34' : 'az/ukwest','35' : 'az/westcentralus','36' : 'az/westeurope','37' : 'az/westus','38' : 'az/westus2','39' : 'gc/asia-east1','40' : 'gc/asia-northeast1','41' : 'gc/asia-southeast1','42' : 'gc/europe-west1','43' : 'gc/us-central1','44' : 'gc/us-east1','45' : 'gc/us-west1' }
+dcIndexMap = {
+  '0': 'aws/ap-northeast-1',
+  '1': 'aws/ap-northeast-2',
+  '2': 'aws/ap-south-1',
+  '3': 'aws/ap-southeast-1',
+  '4': 'aws/ap-southeast-2',
+  '5': 'aws/ca-central-1',
+  '6': 'aws/eu-central-1',
+  '7': 'aws/eu-west-1',
+  '8': 'aws/eu-west-2',
+  '9': 'aws/sa-east-1',
+  '10': 'aws/us-east-1',
+  '11': 'aws/us-east-2',
+  '12': 'aws/us-west-1',
+  '13': 'aws/us-west-2',
+  '14': 'az/australiaeast',
+  '15': 'az/australiasoutheast',
+  '16': 'az/brazilsouth',
+  '17': 'az/canadacentral',
+  '18': 'az/canadaeast',
+  '19': 'az/centralindia',
+  '20': 'az/centralus',
+  '21': 'az/eastasia',
+  '22': 'az/eastus',
+  '23': 'az/eastus2',
+  '24': 'az/japaneast',
+  '25': 'az/japanwest',
+  '26': 'az/koreacentral',
+  '27': 'az/koreasouth',
+  '28': 'az/northcentralus',
+  '29': 'az/northeurope',
+  '30': 'az/southcentralus',
+  '31': 'az/southeastasia',
+  '32': 'az/southindia',
+  '33': 'az/uksouth',
+  '34': 'az/ukwest',
+  '35': 'az/westcentralus',
+  '36': 'az/westeurope',
+  '37': 'az/westus',
+  '38': 'az/westus2',
+  '39': 'gc/asia-east1',
+  '40': 'gc/asia-northeast1',
+  '41': 'gc/asia-southeast1',
+  '42': 'gc/europe-west1',
+  '43': 'gc/us-central1',
+  '44': 'gc/us-east1',
+  '45': 'gc/us-west1',
+}
+
 
 def ReadNetworkLatencies():
 	"""
@@ -20,7 +69,7 @@ def ReadNetworkLatencies():
 	"""
 	print "Reading network latency file..."
 	latencies = {}
-	latencyFile = open(networkLatencies, 'r')
+	latencyFile = open(NET_LATENCIES_LOG, 'r')
 	# errorCount = 0
 	# valueCount = 0
 	for line in latencyFile.readlines():
@@ -35,7 +84,11 @@ def ReadNetworkLatencies():
 				latencies[dc1] = {}
 			if dc2 not in latencies[dc1]:
 				latencies[dc1][dc2] = []
-			latencies[dc1][dc2].append(latency)
+			try:
+				l = float(latency)
+				latencies[dc1][dc2].append(l)
+			except ValueError:
+				pass
 			# valueCount += 1
 
 			# errorCount += 1
@@ -53,31 +106,39 @@ def ReadStorageLatencies():
 	print "Reading storage latency file..."
 	readLatency = {}
 	writeLatency = {}
-	datacenters=listdir(storageLatencies)
+	datacenters = listdir(STORE_LATENCIES_TOP)
 	for dc in datacenters:
-		readStoragefile = open(storageLatencies + dc + "/store-reads.log",'r')
+		readStoragefile = open(STORE_LATENCIES_TOP + dc + STORE_READ_FILE, 'r')
 		for line in readStoragefile.readlines():
 			lineData = line.strip().split()
 			status = lineData[0]
 			latency = lineData[3]
 
 			if status == "success":
-				dcKey = dc.replace("-","/",1)
+				dcKey = dc.replace("-", "/", 1)
 				if dcKey not in readLatency:
 					readLatency[dcKey] = []
-				readLatency[dcKey].append(latency)
+				try:
+					l = float(latency)
+					readLatency[dcKey].append(l)
+				except ValueError:
+					pass
 
-		writeStoragefile = open(storageLatencies + dc + "/store-writes.log",'r')
+		writeStoragefile = open(STORE_LATENCIES_TOP + dc + STORE_WRITE_FILE, 'r')
 		for line in writeStoragefile.readlines():
 			lineData = line.strip().split()
 			status = lineData[0]
 			latency = lineData[3]
 
 			if status == "success":
-				dcKey = dc.replace("-","/",1)
+				dcKey = dc.replace("-", "/", 1)
 				if dcKey not in writeLatency:
 					writeLatency[dcKey] = []
-				writeLatency[dcKey].append(latency)
+				try:
+					l = float(latency)
+					writeLatency[dcKey].append(l)
+				except ValueError:
+					pass
 
 		# if dc == "gc-asia-east1":
 		# 	print len(readLatency[dc])
@@ -98,10 +159,7 @@ def ReadConfigFile(solution_file):
 			items = line.attrib["name"].split("_")
 			yield tuple(items), val
 
-def GetRequestGenerator(networkLatencies, readStorageLatencies, accessSet, listOfReplicas, readQourumSize):
-	randomFrontEnd = random.choice(accessSet)
-	print "The random frontend for get request", randomFrontEnd
-
+def GetRequestGenerator(networkLatencies, readStorageLatencies, randomFrontEnd, listOfReplicas, readQourumSize):
 	sampledNetworkLatencies = {}
 	sampledStorageLatencies = {}
 	totalLinkLatency = {}
@@ -111,17 +169,14 @@ def GetRequestGenerator(networkLatencies, readStorageLatencies, accessSet, listO
 		# print sampledNetworkLatencies[dc], sampledNetworkLatencies[dc]
 		totalLinkLatency[dc] = float(sampledNetworkLatencies[dc]) + float(sampledStorageLatencies[dc])
 
-	readQourum = [ listOfReplicas[i] for i in (random.sample(xrange(len(listOfReplicas)), readQourumSize)) ]
+	readQourum = [listOfReplicas[i] for i in random.sample(xrange(len(listOfReplicas)), readQourumSize)]
 
-	getLatencies = [ totalLinkLatency[i] for i in readQourum ]
+	getLatencies = [totalLinkLatency[i] for i in readQourum]
 
 	getLatencies.sort()
 	return getLatencies[-1]
 
-def PutRequestGenerator(networkLatencies, storageLatencies, accessSet, listOfReplicas, readQourumSize, writeQourumSize):
-	randomFrontEnd = random.choice(accessSet)
-	print "The random frontend for put request", randomFrontEnd
-
+def PutRequestGenerator(networkLatencies, storageLatencies, randomFrontEnd, listOfReplicas, readQourumSize, writeQourumSize):
 	sampledNetworkLatencies = {}
 	sampledStorageLatencies = {}
 	totalLinkLatency = {}
@@ -133,8 +188,8 @@ def PutRequestGenerator(networkLatencies, storageLatencies, accessSet, listOfRep
 		# print sampledNetworkLatencies[dc], sampledNetworkLatencies[dc]
 		totalLinkLatency[dc] = float(sampledNetworkLatencies[dc]) + float(sampledStorageLatencies[dc])
 
-	readQourum = [ listOfReplicas[i] for i in (random.sample(xrange(len(listOfReplicas)), readQourumSize)) ]
-	getLatencies = [ totalLinkLatency[i] for i in readQourum ]
+	readQourum = [listOfReplicas[i] for i in random.sample(xrange(len(listOfReplicas)), readQourumSize)]
+	getLatencies = [totalLinkLatency[i] for i in readQourum]
 	getLatencies.sort()
 	phase1Latency = getLatencies[-1]
 
@@ -148,15 +203,14 @@ def PutRequestGenerator(networkLatencies, storageLatencies, accessSet, listOfRep
 		# print sampledNetworkLatencies[dc], sampledNetworkLatencies[dc]
 		totalLinkLatency[dc] = float(sampledNetworkLatencies[dc]) + float(sampledStorageLatencies[dc])
 
-	writeQourum = [ listOfReplicas[i] for i in (random.sample(xrange(len(listOfReplicas)), writeQourumSize)) ]
-	putLatencies = [ totalLinkLatency[i] for i in writeQourum ]
+	writeQourum = [listOfReplicas[i] for i in random.sample(xrange(len(listOfReplicas)), writeQourumSize)]
+	putLatencies = [totalLinkLatency[i] for i in writeQourum]
 	putLatencies.sort()
- 	phase2Latency = putLatencies[-1]
+	phase2Latency = putLatencies[-1]
 
- 	return phase1Latency + phase2Latency
+	return phase1Latency + phase2Latency
 
 def main():
-
 	if len(sys.argv) < 3:
 		print "Invalid argument"
 		print "Usage: python compare_latencies.py <path to config file> <qourum system | -b,-p,-rd> [number of requests, default=50k]"
@@ -168,7 +222,7 @@ def main():
 		numberOfRequests = int(sys.argv[3])
 
 	networkLatencies = ReadNetworkLatencies()
-	storageLatencies= ReadStorageLatencies()
+	storageLatencies = ReadStorageLatencies()
 	readQourumSize = 0
 	writeQourumSize = 0
 	accessSet = []
@@ -195,15 +249,16 @@ def main():
 	if qourumSystem == "b":
 		outputFileName = 'results/' + 'latency_' + qourumSystem + '_' + str(numberOfRequests)
 		if not os.path.exists(os.path.dirname(outputFileName)):
-		    try:
-		        os.makedirs(os.path.dirname(outputFileName))
-		    except OSError as exc: # Guard against race condition
-		        if exc.errno != errno.EEXIST:
-		            raise
-		outputFile = open(outputFileName,'w')
-		for i in range(numberOfRequests):	
-			sampleGet = GetRequestGenerator(networkLatencies, storageLatencies[0], accessSet, listOfReplicas, readQourumSize)
-			samplePut = PutRequestGenerator(networkLatencies, storageLatencies, accessSet, listOfReplicas, readQourumSize, writeQourumSize)
+			try:
+				os.makedirs(os.path.dirname(outputFileName))
+			except OSError as exc: # Guard against race condition
+				if exc.errno != errno.EEXIST:
+					raise
+		outputFile = open(outputFileName, 'w')
+		for _ in range(numberOfRequests):
+			frontend = random.choice(accessSet)
+			sampleGet = GetRequestGenerator(networkLatencies, storageLatencies[0], frontend, listOfReplicas, readQourumSize)
+			samplePut = PutRequestGenerator(networkLatencies, storageLatencies, frontend, listOfReplicas, readQourumSize, writeQourumSize)
 			outputFile.write("get " + str(sampleGet) + " put " + str(samplePut) + "\n")
 
 
